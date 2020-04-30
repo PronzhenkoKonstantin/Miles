03f960be3c8b1c42f46679604d6bf4910d681f7d
PronzhenkoKonstantin <59308700+PronzhenkoKonstantin@users.noreply.github.com>
2020-04-30 11:12:28 +0300

    main.java and miles.md add

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long price = 1000;
+        int points = 20;
+        long bonus = price / points;
+        System.out.println(bonus);
+    }
+}
